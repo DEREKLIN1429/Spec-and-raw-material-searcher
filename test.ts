f665364f00ff148ab5de370ca10ecb5930eb39ec
DEREKLIN1429 <246640306+DEREKLIN1429@users.noreply.github.com>
2026-03-20 09:24:35 +0530

    Add files via upload

---
+++ test.ts
@@ -0,0 +1,6 @@
+async function run() {
+  const res = await fetch('https://docs.google.com/spreadsheets/d/1_SqDVFnw1xRDCH4MhiWg5ZxUcE1UmryuJ4q-0I23x2s/export?format=csv&gid=0');
+  const text = await res.text();
+  console.log(text.split('\n').filter(l => l.includes('DFA7316FL')).join('\n'));
+}
+run();
